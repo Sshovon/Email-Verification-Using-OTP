5284f68699059a4cb5fa97e1015c1e079214424a
Sshovon <frcshovon@gmail.com>
2023-06-12 19:23:07 +0600

    handling cache failure situation for backup and restore

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -19,14 +19,24 @@ router.post('/backup', upload.single('file'), async (req, res) => {
         const upload = await uploadFile(filePath, key)
 
         deleteFile(filePath)
-        
+
         const cachedUserInfo = await checkCache(key)
-        const jsonParsedCachedUserInfo  = JSON.parse(cachedUserInfo)
-        if(!jsonParsedCachedUserInfo.isBackedup){
-            const result =await User.findOneAndUpdate({email:key},{isBackedup:true},{new:true})
-            await setCache(key,JSON.stringify(result))
+        // handing cache failure
+        if (!cachedUserInfo) {
+            const result = await User.findOneAndUpdate({ email: key }, { isBackedup: true }, { new: true })
+            // console.log(result)
+            await setCache(key, JSON.stringify(result))
+
         }
-                 
+
+        if (cachedUserInfo) {
+            const jsonParsedCachedUserInfo = JSON.parse(cachedUserInfo)
+            if (!jsonParsedCachedUserInfo.isBackedup) {
+                const result = await User.findOneAndUpdate({ email: key }, { isBackedup: true }, { new: true })
+                await setCache(key, JSON.stringify(result))
+            }
+        }
+
         res.status(204).send()
 
     } catch (e) {
@@ -50,23 +60,26 @@ router.post('/backup', upload.single('file'), async (req, res) => {
 router.get('/restore', async (req, res) => {
 
     try {
-        const {email:key} = req.body
-        const cachedUserInfo  = await checkCache(key)
-        
-        if (!cachedUserInfo){
-            const result = await User.find({email:key})
-            if(result.length ===0 ||  result[0].isBackedup === false){
-                return res.status(404).send({message:"No backup found"})
+        const { email: key } = req.body
+        const cachedUserInfo = await checkCache(key)
+
+        // handling cache failure
+        if (!cachedUserInfo) {
+            const result = await User.find({ email: key })
+            if (result.length === 0 || result[0].isBackedup === false) {
+                return res.status(404).send({ message: "No backup found" })
             }
-            if(cachedUserInfo){
-                const jsonParsedCachedUserInfo =  JSON.parse(cachedUserInfo)
-                if(!jsonParsedCachedUserInfo.isBackedup){
-                    return res.status(404).send({message:"No backup found"})
-                }
-            }   
         }
 
-        
+        // has cache but no backup data 
+        if (cachedUserInfo) {
+            const jsonParsedCachedUserInfo = JSON.parse(cachedUserInfo)
+            if (!jsonParsedCachedUserInfo.isBackedup) {
+                return res.status(404).send({ message: "No backup found" })
+            }
+        }
+
+
 
         const upload = downloadFile(key)
         upload.pipe(res)
